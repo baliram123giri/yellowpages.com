1de760bf8b7902e6f1312fed2c457b32b4dc5482
baliram123giri <gir71401@gmail.com>
2022-03-20 18:02:45 +0530

    headerhas-been-don

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // Create a media condition that targets viewports at least 768px wide
 const mediaQuery = window.matchMedia('(min-width: 768px)')
+const mediaQuerymax = window.matchMedia('(max-width: 768px)')
 // Check if the media query is true
 var collpase = document.querySelector(".collapse")
 var collpase2 = document.querySelector(".accordion-collapse1")
@@ -14,4 +15,46 @@ if (mediaQuery.matches) {
   collpase2.classList.add("show")
   collpase3.classList.add("show")
   collpase4.classList.add("show")
+
+  $(document).ready(function(){
+    //here getting mobile content
+   $(".accordion-list").removeClass("accordion-list")
+   
+  
+    var mobileNav =  $("#mobile_aside" ).html()
+    function getContHover (child){
+      var hoverContent = $(`#mobile_aside .${child || "Auto"}`).html()
+         $(".hover_items").html(hoverContent)
+    }
+    getContHover()
+    $(document).on("mouseover",".desktop .first_ele ", function(e){
+        var selector =  !e.target.querySelector("span")?"": e.target.querySelector("span").innerHTML
+            selector = selector.split(" ")[0]
+           $(".desktop .first_ele ").hover(  getContHover(selector))       
+    })
+   
+    $("#desktop_site_nav").html(mobileNav)
+    var deskTopHeadings = $("#desktop_site_nav .answer")
+        deskTopHeadings.hide()
+  })
+  document.getElementById("mobile_login").innerHTML= ""
+  
 }
+
+if(mediaQuerymax.matches){
+ 
+    document.getElementById("mobile_login").innerHTML= $(".user").html()
+    var div = $(".nav_items_box");
+     $("#close").on("click", ()=>{
+        div.animate({right: '-280px'}, "slow");
+        // $(".nav_items_box").addClass("d-none")
+    })
+    $(".menu_bar").on("click", ()=>{
+        div.animate({right: '0px'}, "slow");
+        // $(".nav_items_box").removeClass("d-none")
+    })
+    $("#close").focusout(function() {
+         alert("hello")
+    });
+}
+
